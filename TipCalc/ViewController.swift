ad559931b02dc8a221ebf57b243b9de1c118c337
prazolpp <prazol.pp@gmail.com>
2019-12-18 00:46:19 -0800

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Prajwal Pyakurel on 12/18/19.
+//  Copyright © 2019 Prajwal Pyakurel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
